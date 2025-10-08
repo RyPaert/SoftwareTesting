7f62381e25767914c8659eef16c3dbb931ae4051
RyPaert <158561974+RyPaert@users.noreply.github.com>
2025-10-08 11:44:54 +0300

    done

---
+++ TaltechGuideWebAPI/Controllers/TootedController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TaltechGuideWebAPI.Models;
+
+namespace TaltechGuideWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TootedController : ControllerBase
+    {
+        private static List<Toode> _tooted = new()
+        {
+        new Toode(1,"Koola", 1.5, true),
+        new Toode(2,"Fanta", 1.0, false),
+        new Toode(3,"Sprite", 1.7, true),
+        new Toode(4,"Vichy", 2.0, true),
+        new Toode(5,"Vitamin well", 2.5, true)
+        };
+
+
+        // https://localhost:4444/tooted
+        [HttpGet]
+        public List<Toode> Get()
+        {
+            return _tooted;
+        }
+
+        // https://localhost:4444/tooted/kustuta/0
+        [HttpGet("kustuta/{index}")]
+        public List<Toode> Delete(int index)
+        {
+            _tooted.RemoveAt(index);
+            return _tooted;
+        }
+
+        [HttpGet("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
+        public List<Toode> Add(int id, string nimi, double hind, bool aktiivne)
+        {
+            Toode toode = new Toode(id, nimi, hind, aktiivne);
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        [HttpGet("lisa")] // GET /tooted/lisa?id=1&nimi=Koola&hind=1.5&aktiivne=true
+        public List<Toode> Add2([FromQuery] int id, [FromQuery] string nimi, [FromQuery] double hind, [FromQuery] bool aktiivne)
+        {
+            Toode toode = new Toode(id, nimi, hind, aktiivne);
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        [HttpGet("hind-dollaritesse/{kurss}")] // GET /tooted/hind-dollaritesse/1.5
+        public List<Toode> Dollaritesse(double kurss)
+        {
+            for (int i = 0; i < _tooted.Count; i++)
+            {
+                _tooted[i].Price = _tooted[i].Price * kurss;
+            }
+            return _tooted;
+        }
+
+        // või foreachina:
+
+        [HttpGet("hind-dollaritesse2/{kurss}")] // GET /tooted/hind-dollaritesse2/1.5
+        public List<Toode> Dollaritesse2(double kurss)
+        {
+            foreach (var t in _tooted)
+            {
+                t.Price = t.Price * kurss;
+            }
+
+            return _tooted;
+        }
+
+        [HttpGet("delete-all")]
+        public List<Toode> DeleteAll()
+        {
+            for (int i = 0; i < _tooted.Count; i++)
+            {
+                _tooted.RemoveAt(i);
+            }
+            return _tooted;
+        }
+        [HttpGet("all-activity-false")]
+        public List<Toode> FalseAll()
+        {
+            foreach (var t in _tooted)
+            {
+                t.IsActive = false;
+            }
+            return _tooted;
+        }
+        [HttpGet("get-by-id/{id}")]
+        public List<Toode> GetById(int id)
+        {
+            if (id == )
+        }
+    }
+}
