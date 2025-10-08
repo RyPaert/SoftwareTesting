9a01665cd9d85606bca5f0c3ae7a974ec7ff152b
RyPaert <158561974+RyPaert@users.noreply.github.com>
2025-10-08 11:14:35 +0300

    toode exercises done

---
+++ TaltechGuideWebAPI/Controllers/ToodeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaltechGuideWebAPI.Models;
+
+namespace TaltechGuideWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        [HttpGet("toode")]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+        [HttpGet("changeactivity")]
+        public Toode changeActivity()
+        {
+            if (_toode.IsActive == true)
+            {
+                _toode.IsActive = false;
+            }
+            else
+            {
+                _toode.IsActive = true;
+            }
+            return _toode;
+        }
+        [HttpGet("changename/{name}")]
+        public Toode changeName(string name)
+        {
+            _toode.Name = name;
+            return _toode;
+        }
+        [HttpGet("multiplyprice/{mult}")]
+        public Toode multiplyPrice(int mult)
+        {
+            double price = _toode.Price;
+            double result = price * mult;
+            _toode.Price = result;
+            return _toode;
+        }
+    }
+}
---
+++ TaltechGuideWebAPI/Models/Konto.cs
@@ -0,0 +1,21 @@
+﻿namespace TaltechGuideWebAPI.Models
+{
+    public class Konto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public Konto(Guid id, string name, string password, string firstname, string lastname)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            firstName = firstname;
+            lastName = lastname;
+
+        }
+    }
+}
